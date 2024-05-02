19ea1a5dd4d562ae216497f7fdc72738db4cbaa3
Shimnaldo <shimnaldo98@gmail.com>
2024-05-02 10:04:55 +0900

    feat : 파이어베이스 로그인 인증

---
+++ Assets/01. Scripts/LogInButton.cs
@@ -0,0 +1,42 @@
+using Firebase.Extensions;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LogInButton : MonoBehaviour
+{
+    [SerializeField]
+    private InputField emailInputField;
+
+    [SerializeField]
+    private InputField passInputField;
+
+    public void Login()
+    {
+        string id = emailInputField.text;
+        string pass = passInputField.text;
+
+        FirebaseManager.Auth.SignInWithEmailAndPasswordAsync(id, pass).ContinueWithOnMainThread(task =>
+        {
+            if (task.IsCanceled || task.IsFaulted)
+            {
+                Debug.Log("Sign in task failed");
+                return;
+            }
+
+            Firebase.Auth.AuthResult result = task.Result;
+            Debug.Log($"User signed in successfully: {result.User.DisplayName} ({result.User.UserId})");
+            //if (result.User.IsEmailVerified)
+            //{
+            //    panelController.SetActivePanel(PanelController.Panel.Main);
+            //}
+            //else
+            //{
+            //    panelController.SetActivePanel(PanelController.Panel.Verify);
+            //}
+            //SetInteractable(true);
+        });
+    }
+
+}
